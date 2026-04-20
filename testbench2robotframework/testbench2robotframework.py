30dba198c968bb212e0bd629b6c2c024d0703244
René <snooz@posteo.de>
2026-04-20 12:11:46 +0200

    there were two issues. Please review and fix

--- testbench2robotframework/testbench2robotframework.py
+++ testbench2robotframework/testbench2robotframework.py
@@ -10,8 +10,8 @@ from .testsuite_write import write_test_suites
 from .utils import PathResolver, extract_to_working_directory, is_zip_file
 
 
-def testbench2robotframework(testbench_report: str, config: dict):
-    configuration = Configuration.from_dict(config)
+def testbench2robotframework(testbench_report: str, config: dict | Configuration):
+    configuration = Configuration.from_dict(config) if isinstance(config, dict) else config
     setup_logger(configuration)
     logger.debug("Configuration loaded.")
     testbench_report = Path(testbench_report)
